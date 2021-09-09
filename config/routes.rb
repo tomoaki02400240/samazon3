ade7b41ae804d364d42136a8f855703ec5e8105a
tomoaki02400240 <tokiwakanpou@yahoo.co.jp>
2021-09-09 22:30:57 +0900

    02 create product

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  #get 'products/index'
+  #get 'products/show'
+  #get 'products/new'
+  #get 'products/edit'
+  #get 'products/create'
+  #get 'products/update'
+  #get 'products/destroy'
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_121245) do
+ActiveRecord::Schema.define(version: 2021_09_09_124316) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "major_category_name"
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 2021_09_09_121245) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
